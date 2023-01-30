7622a01586bf927219a70e5cc650a483d1398373
paco-pinot <pinot1207@gmail.com>
2023-01-30 15:22:24 +0100

    Navbar cmc

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+let containerDropdownLatest = document.querySelector(".containerDropdownLatest")
+let dropdownLatest = document.querySelector(".dropdownLatest")
+let btnLatest = document.querySelector(".latestNavbar")
+console.log(containerDropdownLatest,dropdownLatest,btnLatest);
+// btnLatest.addEventListener("mouseover",function () {
+//     containerDropdownLatest.style.display ="flex"
+// })
+
+// dropdownLatest.addEventListener("mouseout",function () {
+//     containerDropdownLatest.style.display ="none"
+// })
